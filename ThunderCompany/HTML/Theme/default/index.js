e38cf4719d95b7fe7d530533e266b1ff1de3fe28
Chichicuil <jorge.uriel1@gmail.com>
2018-03-28 08:04:43 -0600

    added index.js
    Javascript multi sign in not fully operational. Script loads but widget doesn't show.

---
+++ ThunderCompany/HTML/Theme/default/index.js
@@ -0,0 +1,9 @@
+var Nombre = document.getElementById("Nombre");
+var Email = document.getElementById("Email");
+var Contraseña = document.getElementById("Contraseña");
+var Contraseña_Confimarda = document.getElementById("Contraseña_Confimarda");
+var SubmitBtn = document.getElementById("SubmitBtn");
+function SubmitClick()
+{
+window.alert("Works!");
+}
